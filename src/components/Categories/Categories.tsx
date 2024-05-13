37b2f4412f1d7d4311536b2800d656d331d6eabb
BomanStatic <boman.staticsites@gmail.com>
2024-05-13 14:15:23 +0200

    Fix: Categories now gets fetched from the database

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,50 +1,28 @@
-import { Link } from 'react-router-dom';
 import './Categories.css';
+import { Link } from 'react-router-dom';
+import { db } from '../../config/firebase';
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { Category } from '../../types';
+
 export default function Categories() {
-    const mockedCategories = [
-        {
-            name: 'Fiskar',
-            slug: 'fisk',
-            icon: 'fish.svg',
-        },
-        {
-            name: 'Växter',
-            slug: 'vaxt',
-            icon: 'growth.svg',
-        },
-        {
-            name: 'Mat',
-            slug: 'mat',
-            icon: 'food.svg',
-        },
-        {
-            name: 'Akvarium',
-            slug: 'akvarium',
-            icon: 'aquarium.svg',
-        },
-        {
-            name: 'Tillbehör',
-            slug: 'tillbehor',
-            icon: 'accessories.svg',
-        },
-        {
-            name: 'Filter',
-            slug: 'filter',
-            icon: 'filter.svg',
-        },
-        {
-            name: 'Belysning',
-            slug: 'belysning',
-            icon: 'light.svg',
-        },
-    ];
+    const [categories, setCategories] = useState<Category[]>([]);
+    useEffect(() => {
+        const fetchCategories = async () => {
+            const querySnapshot = await getDocs(collection(db, 'category'));
+            const categoryData = querySnapshot.docs.map((doc) => doc.data() as Category);
+            setCategories(categoryData);
+            console.log(categoryData);
+        };
+        fetchCategories();
+    }, []);
     return (
         <>
             <h2 className="categories__title">Kategorier</h2>
             <ul className="categories__list" role="list">
-                {mockedCategories.map((category) => (
+                {categories.map((category) => (
                     <li className="categories__item" key={category.name}>
-                        <img src={`/landing-icons/${category.icon}`} alt="" />
+                        {/* <img src={`/landing-icons/${category.icon}`} alt="" /> */}
                         <Link className="categories__link" to={`kategori/${category.slug}`}>
                             {category.name}
                         </Link>
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface Category {
+    name: string;
+    slug: string;
+    icon: string;
+    title: string;
+}
