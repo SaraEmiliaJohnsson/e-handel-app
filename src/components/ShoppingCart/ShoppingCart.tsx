db435f3ba6f12cf34db954cd1e63762f4caa874b
BomanStatic <boman.staticsites@gmail.com>
2024-05-15 09:28:04 +0200

    Added: Functionality to open and close the shoppingcart

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,15 +1,20 @@
 import ShoppingCartItem from './ShoppingCartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../main';
 import closeIcon from '../../assets/close.svg';
 import { selectTotalPrice } from '../../features/shoppingCartSlice';
 import './ShoppingCart.css';
+import { toggleCart } from '../../features/cartVisibilitySlice';
 export default function ShoppingCart() {
+    const dispatch = useDispatch();
+
     const totalPrice = useSelector(selectTotalPrice) as number;
     const shoppingCart = useSelector((state: RootState) => state.shoppingCart);
+    const cartVisibility = useSelector((state: RootState) => state.cartVisibility);
+
     return (
-        <aside className="shopping-cart">
-            <button type="button" className="shopping-cart__button--close">
+        <aside className={`shopping-cart ${cartVisibility.isCartOpen ? 'open' : ''}`}>
+            <button type="button" onClick={() => dispatch(toggleCart())} className="shopping-cart__button--close">
                 <img src={closeIcon} alt="Close" />
             </button>
             <h2 className="shopping-cart__title">Kundkorg</h2>
--- src/features/rootReducer.ts
+++ src/features/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import shoppingCartReducer from './shoppingCartSlice';
+import cartVisibilitySlice from './cartVisibilitySlice';
 
 export const rootReducer = combineReducers({
     shoppingCart: shoppingCartReducer,
+    cartVisibility: cartVisibilitySlice,
 });
