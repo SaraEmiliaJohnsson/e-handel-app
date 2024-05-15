d07cb97c726a52051dbbcf5c3a1ab92e2b4a2607
Linus Tropp <linustropp@gmail.com>
2024-05-15 08:38:32 +0200

    lagt till lite css

--- src/components/itemlist/ItemListPage.tsx
+++ src/components/itemlist/ItemListPage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ItemList from "../../components/itemlist/ItemList";
 import { useParams } from "react-router-dom";
+import './ItemsList.css'
 
 const ItemsListPage: React.FC = () => {
 	const { slug } = useParams<string>();
 	return (
-	  <div>
-		<h1>{slug} Page</h1>
+	  <div className="centered-container">
+		<h1 className="shop-header">{slug} shop</h1>
 		<ItemList collectionPath={`category/${slug}/items`} />
 	  </div>
 	);
