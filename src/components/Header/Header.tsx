413c1781893ead33fcb2861c70f263996170777d
Linus Tropp <linustropp@gmail.com>
2024-05-15 14:24:34 +0200

    first header draft

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,9 @@ const Header = () => {
 	return (
 		<header className='header-container'>
 			<div className='logo-container'><img src={logo} alt="Logo" className="logo" /><button className='home-button'>Home</button></div>
-			<button className='product-button'>Produkter</button>
-			<button className='login-button'>Logga in</button>
-			<button className='cart-button'>Kundkorg</button>
+			<button className='home-button'>Produkter</button>
+			<button className='home-button'>Logga in</button>
+			<button className='home-button'>Kundkorg</button>
 			</header>
 	)
 }
