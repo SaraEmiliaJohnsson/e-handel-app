859ab286ad1727d9bbb9a322b329519a9855b692
Linus Tropp <linustropp@gmail.com>
2024-05-17 10:02:08 +0200

    header styling osv

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,33 @@
-import './Header.css'
+import './Header.css';
+import '../ShoppingCart/ShoppingCart.css'
 import logo from '../../assets/logo.svg';
+import ShoppingCart from '../ShoppingCart/ShoppingCart';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+	const [isCartOpen, setIsCartOpen] = useState(false);
+
+	const toggleCart = () => {
+		console.log('toggle cart', isCartOpen);
+		setIsCartOpen(!isCartOpen);
+	};
+
+
 	return (
+		<header className='header-background'>
 		<header className='header-container'>
-			<div className='logo-container'><img src={logo} alt="Logo" className="logo" /><button className='home-button'>Home</button></div>
-			<button className='home-button'>Produkter</button>
-			<button className='home-button'>Logga in</button>
-			<button className='home-button'>Kundkorg</button>
+			<div className='logo-container'>
+				<Link to="/">
+					<img src={logo} alt="Logo" className="logo" />
+					</Link>
+					</div>
+					<Link to="/" className='home-button'>Hem</Link>
+			<Link to="/kategori/fish" className='product-button'>Produkter</Link>
+			<Link to="/login" className='login-button'>Logga in</Link>
+			<button onClick={toggleCart} className='cart-button'>Kundkorg</button>
+			<ShoppingCart/>
+			</header>
 			</header>
 	)
 }
