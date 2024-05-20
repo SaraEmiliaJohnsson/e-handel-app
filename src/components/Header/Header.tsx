d82d57a62a760463d169f74f18d5f7cce06dc268
BomanStatic <boman.staticsites@gmail.com>
2024-05-20 13:37:19 +0200

    Fixed: Broken link in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import './Header.css';
-import '../ShoppingCart/ShoppingCart.css'
+import '../ShoppingCart/ShoppingCart.css';
 import logo from '../../assets/logo.svg';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
@@ -9,50 +9,58 @@ import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from '../../config/firebase';
 
 const Header = () => {
-	const dispatch = useDispatch();
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
 
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, (user) => {
-			setIsLoggedIn(!!user);
-		});
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setIsLoggedIn(!!user);
+        });
 
-		return () => unsubscribe();
-	}, []);
+        return () => unsubscribe();
+    }, []);
 
-	const handleLogout = async () => {
-		try {
-			await signOut(auth);
-			console.log('Utloggad');
-			navigate('/');
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            console.log('Utloggad');
+            navigate('/');
+        } catch (error) {
+            console.error('Error logging out', error);
+        }
+    };
 
-		} catch (error) {
-			console.error('Error logging out', error);
-		}
-	};
+    return (
+        <header className="header-background">
+            <header className="header-container">
+                <div className="logo-container">
+                    <Link to="/">
+                        <img src={logo} alt="Logo" className="logo" />
+                    </Link>
+                </div>
+                <Link to="/" className="home-button">
+                    Hem
+                </Link>
+                <Link to="/kategorier" className="product-button">
+                    Produkter
+                </Link>
+                {isLoggedIn ? (
+                    <button className="login-button" onClick={handleLogout}>
+                        Logga ut
+                    </button>
+                ) : (
+                    <Link to="/login" className="login-button">
+                        Logga in
+                    </Link>
+                )}
 
-
-	return (
-		<header className='header-background'>
-			<header className='header-container'>
-				<div className='logo-container'>
-					<Link to="/">
-						<img src={logo} alt="Logo" className="logo" />
-					</Link>
-				</div>
-				<Link to="/" className='home-button'>Hem</Link>
-				<Link to="/kategori/fish" className='product-button'>Produkter</Link>
-				{isLoggedIn ? (
-					<button className='login-button' onClick={handleLogout}>Logga ut</button>
-				) : (
-					<Link to="/login" className='login-button'>Logga in</Link>
-				)}
-
-				<button type="button" className='cart-button' onClick={() => dispatch(toggleCart())}>Kundkorg</button>
-			</header>
-		</header>
-	)
-}
+                <button type="button" className="cart-button" onClick={() => dispatch(toggleCart())}>
+                    Kundkorg
+                </button>
+            </header>
+        </header>
+    );
+};
 
 export default Header;
