81e79a775b3cc1700ecd6588bd8fe740ad721578
Linus Tropp <linustropp@gmail.com>
2024-05-22 11:19:22 +0200

    more deletions

--- src/pages/CategoryPage/CategoryPage.tsx
+++ src/pages/CategoryPage/CategoryPage.tsx
@@ -1,20 +1,8 @@
-import { useEffect, useState } from "react";
 import "./CategoryPage.css"
-import { Category } from "../../types";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../config/firebase";
 import Categories from "../../components/Categories/Categories";
 
 export default function CategoryPage() {
-	const [categories , setCategories] = useState<Category[]>([]);
-    useEffect(() => {
-        const fetchCategories = async () => {
-            const querySnapshot = await getDocs(collection(db, 'category'));
-            const categoryData = querySnapshot.docs.map((doc) => ({ slug: doc.id, ...doc.data() } as Category));
-            setCategories(categoryData);
-        };
-        fetchCategories();
-    }, []);
+	
     return <div className="category-container">
 		<Categories/>
 		
