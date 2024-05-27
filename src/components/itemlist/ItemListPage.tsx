494e08373d92eef7f621b53a89d0fefafa44dab0
Linus Tropp <linustropp@gmail.com>
2024-05-27 12:17:40 +0200

    back knapp

--- src/components/itemlist/ItemListPage.tsx
+++ src/components/itemlist/ItemListPage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ItemList from "../../components/itemlist/ItemList";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import './ItemsList.css'
 
 const ItemsListPage: React.FC = () => {
 	const { slug } = useParams<string>();
 	return (
 	  <div className="centered-container">
+		<Link to="/kategorier" className="back-button">
+                    Back
+                </Link>
 		<h1 className="shop-header">{slug}</h1>
 		<ItemList collectionPath={`category/${slug}/items`} />
 	  </div>
