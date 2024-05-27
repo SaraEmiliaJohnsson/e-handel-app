3a3fa2f6df06a4ee7a45f2b37ebefefd313bc9dd
Linus Tropp <linustropp@gmail.com>
2024-05-27 13:52:04 +0200

    fixade tillbakapil

--- src/components/itemlist/ItemListPage.tsx
+++ src/components/itemlist/ItemListPage.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import ItemList from "../../components/itemlist/ItemList";
 import { Link, useParams } from "react-router-dom";
 import './ItemsList.css'
+import backarrow from "../../assets/backarrow.svg"
 
 const ItemsListPage: React.FC = () => {
 	const { slug } = useParams<string>();
 	return (
 	  <div className="centered-container">
 		<Link to="/kategorier" className="back-button">
-                    Back
+		<img src={backarrow} alt="back arrow" />
                 </Link>
 		<h1 className="shop-header">{slug}</h1>
 		<ItemList collectionPath={`category/${slug}/items`} />
